76c36c24132c5357bad5d47061d68f8f01cdfcff
programadormovel <programadormovel@gmail.com>
2025-09-19 09:31:55 -0300

    listagem estática

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import { useState } from "react"
+import Produtos from "./pages/produtos"
+const App = () => {
 
-function App(){
-
-  const [nome, setNome] = useState('Ronaldo')
 
   return (
-    <div>
-      <h3>Pizzaria 2D</h3>
+    <>
+      <h1>Projeto Pizzaria 2D</h1>
+      <Produtos />
 
-      <input
-        className="nome"
-        onChange={(e)=>{setNome(e.target.value)}}
-        type="text" />
-
-      <button
-        className="botao"
-        onClick={()=>{
-          alert('O nome digita foi ' + nome)
-        }
-        }
-      >
-        CLIQUE AQUI
-      </button>
-
-    </div>
+    </>
   )
 }
-
 export default App
