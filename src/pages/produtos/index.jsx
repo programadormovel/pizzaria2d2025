501d6d1a2017f0493c4e52d877f6072fb69cb59f
programadormovel <programadormovel@gmail.com>
2025-09-25 11:49:06 -0300

    consumo de rotas

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,13 +1,21 @@
+// ferramenta de consumo de rotas ou endpoints do backend
 import axios from "axios"
+// Hooks do react para controlar estados e renderização
 import { useState, useEffect } from "react"
 const Produtos = () => {
+    // estado com lista de pizzas
     const [pizzas, setPizzas] = useState([])
     // Objeto com a lista de pizzas
     useEffect(()=>{
+        // GET - busca os dados da rota ou endpoint
+        // then - método assíncrono, espera o retorno da requisição
+        // catch - tratamento de erros da requisição
         axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
         .then(response=>{
+            console.log(response.data.data)
             setPizzas(response.data.data)
         })
+        .catch(errors=>console.log(errors))
     }, [])
 
     // Iteração da lista de pizzas 
